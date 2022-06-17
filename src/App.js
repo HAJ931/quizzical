9e0bd8a7dd529f476fe19667d7c033b2314172c7
HAJ931 <63355662+HAJ931@users.noreply.github.com>
2022-06-17 21:58:07 +0530

    added home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react"
+import Footer from "./components/Footer"
 
 export default function App() {
     return (
-        <main>
-            <h1>Quizzical</h1>
-        </main>
+        <>
+            <main>
+                <h1 className="main--title">Quizzical</h1>
+                <h5 className="main--subtitle">
+                    A fun quiz app to test your trivia skills!
+                </h5>
+                <button className="main--button">
+                    Start Game
+                </button>
+            </main>
+            <Footer />
+        </>
     )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+// import '../index.css'
+
+export default function Footer() {
+    return(
+        <footer>
+            <h1 className="footer--content">    
+                Made with 💜 by Hanan Abdul Jaleel
+            </h1>
+        </footer>
+    )
+}
