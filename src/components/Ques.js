c6fe1cea9a984f7b76b94e108d710940df03fe7e
HAJ931 <63355662+HAJ931@users.noreply.github.com>
2022-06-20 21:31:53 +0530

    feat(submission):implemented check answers functionality and completed MVP 1

--- src/components/Ques.js
+++ src/components/Ques.js
@@ -1,8 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 
 import "./Ques.css"
 
 export default function Ques(props) {
+
+    const [ selectedOption, setSelectedOption ] = useState(-1)
+
+    function setOptionClass(index, b){
+        let str = 'trivia--option'
+        let extra = ''
+        if(props.isSubmitted){
+            if(index === selectedOption && !b)
+                extra = 'trivia--wrong inactive'
+            else if(b)
+                extra = 'trivia--right'
+            else
+                extra = 'inactive'
+        }
+        else {
+            extra = selectedOption === index ? 'active': ""
+        }
+        //console.log(`${str} ${extra}`)
+        return `${str} ${extra}`
+    }
+
+    //console.log(props.answers)
+
+    const optionElements = props.answers.map(
+        (ans, index) => 
+        <li 
+            key={index} 
+            id={index} 
+            className={setOptionClass(index, ans.is_correct)} 
+            onClick={() => {!props.isSubmitted && setSelectedOption(selectedOption === index ? -1: index)}}>{ans.value}</li>
+    )
     
     return (
         <div className="trivia">
@@ -11,9 +42,7 @@ export default function Ques(props) {
             </div>
             <ul className="trivia--answers">
                 {
-                    props.answers.map(
-                        ans => <li className="trivia--option">{ans.value}</li>
-                    )
+                    optionElements
                 }
             </ul>
             <hr className="trivia--divider"></hr>
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,10 +2,31 @@ import React, { useState, useEffect } from "react"
 
 import "./Game.css"
 import Ques from "../components/Ques"
+import Footer from "../components/Footer"
 
 export default function Game() {
 
     const [ quizQues, setQuizQues ] = useState([])
+    const [ score, setScore ] = useState(0)
+    const [ isSubmitted, setIsSubmitted ] = useState(false)
+
+    function shuffleArr(array) {
+        let currentIndex = array.length,  randomIndex;
+      
+        // While there remain elements to shuffle.
+        while (currentIndex !== 0) {
+      
+          // Pick a remaining element.
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+      
+        return array;
+      }
 
     // eslint-disable-next-line
     useEffect( 
@@ -14,11 +35,11 @@ export default function Game() {
                 .then(res => res.json())
                     .then(data => setQuizQues(data.results.map(
                         quiz => ({question: quiz.question, 
-                            answers: [
+                            answers: shuffleArr([
                                 {value: quiz.correct_answer, is_correct: true}, 
                                 ...quiz.incorrect_answers.map
                                     (inc => ({value: inc, is_correct: false}))
-                                ]
+                                ]),
                             })
                     )))
         }, [])
@@ -32,15 +53,55 @@ export default function Game() {
     const questionElements = quizQues.map( (ques, index) =>
         <Ques 
             key={index} 
+            id={index}
             question={decodeHtml(ques.question).replace(/&#?\w+;/, match => entities[match])} 
             answers={ques.answers.map(ans=>({value: decodeHtml(ans.value), is_correct: ans.is_correct}))}
+            isSubmitted={isSubmitted}
         />
     )
 
+    function submitAns(){
+        const elems = document.getElementsByClassName('active');
+        let count = 0
+        if(elems.length < 5){
+            alert("Please select the answers to all the questions!")
+            return
+        }
+        for (let i = 0; i < 5; ++i){
+            if(quizQues[i].answers[elems[i].id].is_correct)
+                ++count;
+        }
+        setIsSubmitted(prevVal => !prevVal)
+        setScore(count)
+        //alert(`You got ${count} / 5 correct!`)
+    }
+
     return(
-        <div className="game">
-            { questionElements }
-        <button className="game--button">Check Answers</button>
-        </div>
+        <>
+            <div className="game">
+                { questionElements }
+            <div className="game--results">
+                {
+                    isSubmitted ?
+                    <>
+                        <p className="game--score">You scored {score} / 5 correct answers</p>
+                            <button 
+                                className="game--reset" 
+                                onClick={() => window.location.reload()}
+                                >
+                                    Play Again
+                            </button>
+                    </>: 
+                    <button 
+                        className="game--button" 
+                        onClick={submitAns}
+                    >
+                            Check Answers
+                    </button>
+                    }
+            </div>
+            </div>
+            <Footer />    
+        </>
     )
 }
